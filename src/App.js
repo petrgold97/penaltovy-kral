3ea793ad1b5866853479b322904dfbda7a7ffe74
petrgold97 <gold3@seznam.cz>
2025-04-26 16:08:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-const YOUTUBE_EMBED_URL = "https://www.youtube.com/live/sqYTKvtzyw8";
+const YOUTUBE_EMBED_URL = "https://www.youtube.com/live/sqYTKvtzyw8?si=e1OC-vGa_NzV8REp";
 const GOOGLE_SHEET_API = "https://script.google.com/macros/s/AKfycbyIQXUr5qMOYEHo_dcURbLEn-6ywZMaeyFSpT88G1xTTvqCvRGQbu1996iWAOEYVNnHzw/exec";
 
 function App() {
