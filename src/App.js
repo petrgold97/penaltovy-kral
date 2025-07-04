5b47ea43555b8406a8f5a028fc313b1e3f34342b
petrgold97 <gold3@seznam.cz>
2025-07-04 20:59:44 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -97,12 +97,12 @@ function App() {
             <div className="carousel-track">
               <img src="/1.png" alt="1.png" />
               <img src="/2.png" alt="2.png" />
-              <img src="/3.png" alt="3.png" />
+              <img src="/3.jpg" alt="3.png" />
               <img src="/4.png" alt="4.png" />
               <img src="/5.png" alt="5.png" />
               <img src="/6.png" alt="6.png" />
               <img src="/7.png" alt="7.png" />
-              <img src="/8.png" alt="8.png" />
+              <img src="/8.jpg" alt="8.png" />
               <img src="/9.png" alt="9.png" />
               <img src="/10.png" alt="10.png" />
               <img src="/11.png" alt="11.png" />
